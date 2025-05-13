664b4f3ebced792b23c5fd05b42aea101bacbad7
JS-404-000 <jindate2@gmail.com>
2025-05-13 11:29:43 +0900

    commit

--- app/src/main/java/com/example/clayshootinggame/MainActivity.java
+++ app/src/main/java/com/example/clayshootinggame/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.clayshootinggame;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -43,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
 
         Button btnStart = findViewById(R.id.btnStart);
         Button btnStop = findViewById(R.id.btnStop);
@@ -105,15 +111,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     private void shootingStart() {
+        float bullet_x = gun_center_x - bullet_width/2;
+        iv_bullet.setX(bullet_x);
+        iv_bullet.setVisibility(View.VISIBLE);
+        ObjectAnimator bullet_scaleDownX = ObjectAnimator.ofFloat(iv_bullet, "scaleX", 1.0f, 0f);
+        ObjectAnimator bullet_scaleDownY = ObjectAnimator.ofFloat(iv_bullet, "scaleY", 1.0f, 0f);
+        ObjectAnimator bullet_translateX = ObjectAnimator.ofFloat(iv_bullet, "translationX", (float)bullet_x, (float)bullet_x);
+        ObjectAnimator bullet_translateY = ObjectAnimator.ofFloat(iv_bullet, "translationY", gun_y, 0f);
+        bullet_translateY.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                bullet_center_x = iv_bullet.getX() + bullet_width*0.5f;
+                bullet_center_y = iv_bullet.getY() + bullet_height*0.5f;
+                clay_center_x = iv_clay.getX() + clay_width*0.5f;
+                clay_center_y = iv_clay.getY() + clay_height*0.5f;
+                double dist=Math.sqrt(Math.pow(bullet_center_x-clay_center_x, 2)+Math.pow(bullet_center_y-clay_center_y, 2));
+                if (dist <= 100) { iv_clay.setVisibility(View.INVISIBLE); }
+            }
+        });
+        AnimatorSet bullet = new AnimatorSet();
+        bullet.playTogether(bullet_translateX, bullet_translateY, bullet_scaleDownX, bullet_scaleDownY);
+        bullet.start();
     }
+
 
     private void gameStop() {
         finish();
     }
 
     private void gameStart() {
+        ObjectAnimator clay_translateX = ObjectAnimator.ofFloat(iv_clay, "translationX", -100f, (float)screen_width);
+        ObjectAnimator clay_translateY = ObjectAnimator.ofFloat(iv_clay, "translationY", 0f, 0f );
+        ObjectAnimator clay_rotation = ObjectAnimator.ofFloat(iv_clay, "rotation", 0f, 360f*5f);
+        clay_translateX.setRepeatCount(NO_OF_CLAYS-1);
+        clay_translateY.setRepeatCount(NO_OF_CLAYS-1);
+        clay_rotation.setRepeatCount(NO_OF_CLAYS-1);
+        clay_translateX.setDuration(3000);
+        clay_translateY.setDuration(3000);
+        clay_rotation.setDuration(3000);
+        clay_translateX.addListener(new Animator.AnimatorListener() {
+            public void onAnimationStart(Animator animator) {
+                iv_clay.setVisibility(View.VISIBLE);
+            }
+            public void onAnimationEnd(Animator animator) {
+                Toast.makeText(getApplicationContext(), "게임 종료", Toast.LENGTH_SHORT).show();
+            }
+            public void onAnimationCancel(Animator animator) {
+
+            }
+            public void onAnimationRepeat(Animator animator) {
+                iv_clay.setVisibility(View.VISIBLE);
+            }
+        });
+        clay_translateX.start();
+        clay_translateY.start();
+        clay_rotation.start();
+    }
 
     }
 
-}
-}
+
